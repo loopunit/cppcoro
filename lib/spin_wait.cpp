242db2b0f6d2c485e3f869b284ff1962071b1eba
Nathan Rausch <1158984+loopunit@users.noreply.github.com>
2019-03-20 23:10:12 -0700

    Fixing windows static thread pool destructor deadlock.
    On a windows build, the thread pool destructor will deadlock after a period of idle time. This is happening because m_count has become sufficiently large that the YieldProcessor loop will block for minutes at a time.
    
    Flipping the next_spin_will_yield check fixes this behavior, and I imagine was the intended behavior in the first place. This results in the lighter YieldProcessor calls being hit first, followed by the heavier OS Sleep calls, which continue until the spin resets or m_count wraps.

--- lib/spin_wait.cpp
+++ lib/spin_wait.cpp
@@ -45,7 +45,7 @@ namespace cppcoro
 #if CPPCORO_OS_WINNT
 		// Spin strategy taken from .NET System.SpinWait class.
 		// I assume the Microsoft guys knew what they're doing.
-		if (next_spin_will_yield())
+		if (!next_spin_will_yield())
 		{
 			// CPU-level pause
 			// Allow other hyper-threads to run while we busy-wait.
